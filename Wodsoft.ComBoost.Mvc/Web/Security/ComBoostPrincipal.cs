436500f16ac70fbddc27967082ad14005cd016cc
Kation <lwcqq@163.com>
2014-05-15 17:38:50 +0800

    update identity

--- Wodsoft.ComBoost.Mvc/Web/Security/ComBoostPrincipal.cs
+++ Wodsoft.ComBoost.Mvc/Web/Security/ComBoostPrincipal.cs
@@ -16,34 +16,49 @@ namespace System.Web.Security
             if (user == null)
                 throw new ArgumentNullException("user");
             OriginPrincipal = user;
+            Identity = new ComBoostIdentity(this);
         }
 
         public IPrincipal OriginPrincipal { get; private set; }
 
-        public IIdentity Identity
-        {
-            get { return OriginPrincipal.Identity; }
-        }
+        public IIdentity Identity { get; private set; }
 
         public static RoleEntityResolveDelegate Resolve { get; set; }
 
+        public IRoleEntity RoleEntity { get; private set; }
+        private bool _IsFailure;
+
+        internal bool InitRoleEntity()
+        {
+            if (_IsFailure)
+                return false;
+            if (RoleEntity != null)
+                return true;
+            RouteData routeData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(HttpContext.Current));
+            EntityRoute route = routeData.Route as EntityRoute;
+            if (route == null)
+            {
+                _IsFailure = true;
+                return false;
+            }
+            RoleEntity = Resolve(route.UserType, Identity.Name);
+            if (RoleEntity == null)
+            {
+                _IsFailure = true;
+                return false;
+            }
+            return true;
+        }
+
         public bool IsInRole(string role)
         {
+            if (!OriginPrincipal.Identity.IsAuthenticated)
+                return false;
             if (Resolve == null)
                 return OriginPrincipal.IsInRole(role);
-            IRoleEntity entity = HttpContext.Current.Items["RoleEntity"] as IRoleEntity;
-            if (entity == null)
-            {
-                RouteData routeData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(HttpContext.Current));
-                EntityRoute route = routeData.Route as EntityRoute;
-                if (route == null)
-                    return OriginPrincipal.IsInRole(role);
-                entity = Resolve(route.UserType, Identity.Name);
-                if (entity == null)
-                    return false;
-                HttpContext.Current.Items["RoleEntity"] = entity;
-            }
-            return entity.IsInRole(role);
+            if (!InitRoleEntity())
+                return false;
+            return RoleEntity.IsInRole(role);
         }
     }
 
