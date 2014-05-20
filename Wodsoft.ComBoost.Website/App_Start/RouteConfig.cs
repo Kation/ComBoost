ebf3f43036f04799d356bd860b3a6774ae4e74f6
Kation <lwcqq@163.com>
2014-05-20 17:59:16 +0800

    add license page

--- Wodsoft.ComBoost.Website/App_Start/RouteConfig.cs
+++ Wodsoft.ComBoost.Website/App_Start/RouteConfig.cs
@@ -12,14 +12,16 @@ namespace Wodsoft.ComBoost.Website
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
+            routes.MapRoute("Home", "", new { controller = "Home", action = "Init" });
+
+            routes.MapRoute("License", "license", new { controller = "Home", action = "License" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{lang}/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            routes.MapRoute("Home", "", new { controller = "Home", action = "Init" });
         }
     }
 }
