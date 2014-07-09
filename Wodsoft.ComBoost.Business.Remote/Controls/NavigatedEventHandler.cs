95544e19aae3d1622bd838058987c7272b49b20d
Kation <lwcqq@163.com>
2014-07-09 18:43:03 +0800

    no use

--- Wodsoft.ComBoost.Business.Remote/Controls/NavigatedEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Wodsoft.ComBoost.Business.Controls
-{
-    public delegate void NavigatedEventHandler(WorkPage oldPage,WorkPage newPage);
-}
