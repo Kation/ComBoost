cb7341ca44ec8043ded2ada238f4e9f421ab26f9
Kation <lwcqq@163.com>
2014-09-04 14:09:39 +0800

    add pagination class.

---
+++ Wodsoft.ComBoost/ComponentModel/Pagination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.ComponentModel
+{
+    public class Pagination : IPagination
+    {
+        public Pagination()
+        {
+            PageSizeOption = EntityViewModel.DefaultPageSizeOption;
+        }
+
+        public Pagination(int page, int count, int size)
+            : this()
+        {
+            CurrentPage = page;
+            CurrentSize = size;
+            TotalPage = (int)Math.Ceiling(count / (double)size);
+        }
+
+        public Pagination(int page, int count)
+            : this(page, count, EntityViewModel.DefaultPageSize) { }
+
+        public int[] PageSizeOption { get; set; }
+
+        public int TotalPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int CurrentSize { get; set; }
+    }
+}
