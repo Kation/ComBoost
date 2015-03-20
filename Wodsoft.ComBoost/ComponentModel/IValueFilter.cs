357b6205ec0ebf0a860e81e0ce50475a8e591d9d
Kation <lwcqq@163.com>
2015-03-20 16:15:09 +0800

    update

---
+++ Wodsoft.ComBoost/ComponentModel/IValueFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.ComponentModel
+{
+    /// <summary>
+    /// Value filter interface.
+    /// </summary>
+    public interface IValueFilter
+    {
+        /// <summary>
+        /// Get values.
+        /// </summary>
+        /// <param name="dependencyProperty">Dependency property name.</param>
+        /// <param name="dependencyValue">Dependency property value.</param>
+        /// <returns></returns>
+        NameValueCollection GetValues(string dependencyProperty, string dependencyValue);
+    }
+}
