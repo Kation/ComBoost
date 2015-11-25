96e79c74739eb220dc7dc119426b1409161d4d7f
Kation <lwcqq@163.com>
2015-11-25 20:30:21 +0800

    add extension

---
+++ Wodsoft.ComBoost.EntityFramework/Data/Entity/EntityExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Data.Entity
+{
+    public static class EntityExtensions
+    {
+        public static DbContext GetContext(IEntityContextBuilder builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException("builder");
+            if (builder is EntityContextBuilder)
+                return ((EntityContextBuilder)builder).DbContext;
+            return null;
+        }
+    }
+}
