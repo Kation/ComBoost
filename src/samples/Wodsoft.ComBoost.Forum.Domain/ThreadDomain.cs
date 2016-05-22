e4992d2db25467d5bf65b57f1d1c725e48b43b4e
Kation <lwcqq@163.com>
2016-05-23 00:20:23 +0800

    更新示例项目

--- src/samples/Wodsoft.ComBoost.Forum.Domain/ThreadDomain.cs
+++ src/samples/Wodsoft.ComBoost.Forum.Domain/ThreadDomain.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wodsoft.ComBoost.Data;
 
 namespace Wodsoft.ComBoost.Forum.Domain
 {
-    public class ThreadDomain
+    public class ThreadDomain : DomainService
     {
-        public async Task Create(IDomainContext serviceContext)
+        public async Task Create([FromService] IAuthenticationProvider authentication, string title, string content)
         {
-
+            //IAuthenticationProvider
+            //EntityDomain<>
         }
     }
 }
