a41feece3421ef5606dca235bbaffc08933d1e96
Kation L <kation@outlook.com>
2016-05-23 18:11:51 +0800

    添加权限提供器接口方法

--- src/Wodsoft.ComBoost.Security/IAuthenticationProvider.cs
+++ src/Wodsoft.ComBoost.Security/IAuthenticationProvider.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using Wodsoft.ComBoost.Security;
 
 namespace Wodsoft.ComBoost
 {
     public interface IAuthenticationProvider
     {
+        IAuthentication GetAuthentication();
     }
 }
