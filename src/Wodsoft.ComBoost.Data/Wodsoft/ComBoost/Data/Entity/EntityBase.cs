ff0fb385e747edc2fd81e4453a34fda4b2683942
Kation L <kation@outlook.com>
2016-05-23 18:12:52 +0800

    添加实体接口与默认实现

---
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/Entity/EntityBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost.Data.Entity
+{
+    public class EntityBase : IEntity
+    {
+        public DateTime CreateDate { get; set; }
+
+        [Key]
+        public Guid Index { get; set; }
+
+        object IEntity.Index { get { return Index; } set { Index = (Guid)value; } }
+
+        public virtual bool IsEditAllowed { get { return true; } }
+
+        public virtual bool IsRemoveAllowed { get { return true; } }
+
+        public virtual void OnCreateCompleted() { }
+
+        public virtual void OnEditCompleted() { }
+
+        public virtual void OnPreCreate()
+        {
+            CreateDate = DateTime.Now;
+        }
+
+        public virtual void OnPreEdit() { }
+    }
+}
---
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/Entity/IEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost.Data.Entity
+{
+    public interface IEntity
+    {
+        object Index { get; set; }
+
+        DateTime CreateDate { get; set; }
+
+        void OnPreCreate();
+
+        void OnCreateCompleted();
+
+        void OnPreEdit();
+
+        void OnEditCompleted();
+
+        bool IsRemoveAllowed { get; }
+
+        bool IsEditAllowed { get; }
+    }
+}
