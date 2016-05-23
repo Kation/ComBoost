738c347ed4480821801ff598dfc28e7238c95f85
Kation L <kation@outlook.com>
2016-05-23 18:10:56 +0800

    添加值提供器扩展

---
+++ src/Wodsoft.ComBoost.Core/ValueProviderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost
+{
+    public static class ValueProviderExtensions
+    {
+        public static object GetRequiredValue(this IValueProvider valueProvider, string name)
+        {
+            object value = valueProvider.GetValue(name);
+            if (value == null)
+                throw new ArgumentException("找不到所需值。");
+            return value;
+        }
+
+        public static T GetRequriedValue<T>(this IValueProvider valueProvider, string name)
+        {
+            T value = valueProvider.GetValue<T>(name);
+            if (value == null)
+                throw new ArgumentException("找不到所需值。");
+            return value;
+        }
+    }
+}
