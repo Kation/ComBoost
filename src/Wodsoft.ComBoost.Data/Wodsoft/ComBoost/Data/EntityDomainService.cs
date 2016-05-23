6ed11f8f2199570cb7886cc3e3ab3db611000296
Kation L <kation@outlook.com>
2016-05-23 18:13:08 +0800

    更新实体领域服务

--- src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/EntityDomainService.cs
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/EntityDomainService.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Wodsoft.ComBoost.Data.Entity;
 using Microsoft.Extensions.DependencyInjection;
 using System.ComponentModel;
+using Wodsoft.ComBoost.Data.Entity.Metadata;
 
 namespace Wodsoft.ComBoost.Data
 {
     public class EntityDomainService<T> : DomainService
         where T : class, new()
     {
+        public IEntityMetadata Metadata { get; private set; }
+
+        public EntityDomainService()
+        {
+            Metadata = EntityAnalyzer.GetMetadata<T>();
+        }
+
         public virtual Task Create([FromService] IDatabaseContext database)
         {
             var context = database.GetContext<T>();
@@ -22,16 +30,26 @@ namespace Wodsoft.ComBoost.Data
         public virtual async Task Update([FromService] IDatabaseContext database, [FromService] IValueProvider valueProvider)
         {
             var context = database.GetContext<T>();
-
-            ExecutionContext.DomainContext.Result = await database.SaveAsync();
+            object index = valueProvider.GetRequiredValue(Metadata.KeyProperty.ClrName);
+            if (index.GetType() != Metadata.KeyProperty.ClrType)
+            {
+                var converter = TypeDescriptor.GetConverter(Metadata.KeyProperty.ClrType);
+                index = converter.ConvertFrom(index);
+            }
+            var entity = await context.GetAsync(index);
+            if (entity == null)
+                return;
+            await UpdateCore(valueProvider, entity);
+            //ExecutionContext.DomainContext.Result = await database.SaveAsync();
         }
 
-        protected virtual Task UpdateCore(IDomainContext serviceContext, T entity)
+        protected virtual Task UpdateCore(IValueProvider valueProvider, T entity)
         {
+            //Metadata.
             return null;
         }
 
-        protected virtual Task UpdateProperty(IDomainContext serviceContext, T entity)
+        protected virtual Task UpdateProperty(IValueProvider valueProvider, T entity)
         {
             return null;
 
