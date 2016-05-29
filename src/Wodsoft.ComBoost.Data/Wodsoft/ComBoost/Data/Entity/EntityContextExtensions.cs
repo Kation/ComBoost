6c38ee370052f7b5cd7f91d2a5f263a417e32d28
Kation <lwcqq@163.com>
2016-05-30 01:13:09 +0800

    分离扩展

---
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/Entity/EntityContextExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using Wodsoft.ComBoost.Data.Entity.Metadata;
+
+namespace Wodsoft.ComBoost.Data.Entity
+{
+    public static class EntityContextExtensions
+    {
+        public static Task<T> GetAsync<T>(this IEntityContext<T> context, object key)
+            where T : class, new()
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            var parameter = Expression.Parameter(context.Metadata.Type);
+            var property = Expression.Property(parameter, context.Metadata.KeyProperty.ClrName);
+            var expression = Expression.Equal(property, Expression.Constant(key));
+            var lambda = Expression.Lambda<Func<T, bool>>(expression, parameter);
+            return context.SingleOrDefaultAsync(context.Query(), lambda);
+        }
+
+        public static IOrderedQueryable<T> Order<T>(this IEntityContext<T> context)
+            where T : class, new()
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            return Order(context, context.Query());
+        }
+
+        public static IOrderedQueryable<T> Order<T>(this IEntityContext<T> context, IQueryable<T> query)
+            where T : class, new()
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (query == null)
+                throw new ArgumentNullException(nameof(query));
+            var parameter = Expression.Parameter(typeof(T));
+            IPropertyMetadata sortProperty = context.Metadata.SortProperty ?? context.Metadata.KeyProperty;
+            dynamic express = Expression.Lambda(typeof(Func<,>).MakeGenericType(typeof(T), sortProperty.ClrType), Expression.Property(parameter, sortProperty.ClrName), parameter);
+            if (context.Metadata.SortDescending)
+                return Queryable.OrderByDescending(query, express);
+            else
+                return Queryable.OrderBy(query, express);
+        }
+
+        public static IQueryable<T> InParent<T>(this IEntityContext<T> context, IQueryable<T> query, string path, object value)
+            where T : class, new()
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (query == null)
+                throw new ArgumentNullException(nameof(query));
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            var parameter = Expression.Parameter(typeof(T));
+            MemberExpression member = null;
+            string[] properties = path.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            Type type = context.Metadata.Type;
+            for (int i = 0; i < properties.Length; i++)
+            {
+                PropertyInfo property = type.GetProperty(properties[i]);
+                if (property == null)
+                    throw new ArgumentException("父级路径错误。");
+                if (member == null)
+                    member = Expression.Property(parameter, property);
+                else
+                    member = Expression.Property(member, property);
+                type = property.PropertyType;
+            }
+            Expression equal;
+            if (type.GetTypeInfo().IsValueType)
+                equal = Expression.Equal(member, Expression.Constant(value));
+            else
+            {
+                var propertyMetadata = EntityDescriptor.GetMetadata(type);
+                equal = Expression.Equal(Expression.Property(member, type.GetProperty(propertyMetadata.KeyProperty.ClrName)), Expression.Constant(value));
+            }
+            var express = Expression.Lambda<Func<T, bool>>(equal, parameter);
+            return query.Where(express);
+        }
+
+        public static IQueryable<T> InParent<T>(this IEntityContext<T> context, IQueryable<T> query, object[] parentIds)
+            where T : class, new()
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (query == null)
+                throw new ArgumentNullException(nameof(query));
+            if (parentIds == null || parentIds.Length == 0)
+                throw new ArgumentNullException(nameof(parentIds));
+            if (context.Metadata.ParentProperty == null)
+                throw new NotSupportedException("该实体不支持父级元素。");
+            var parameter = Expression.Parameter(typeof(T));
+            Expression equal = null;
+            foreach (object parent in parentIds)
+            {
+                var item = Expression.Equal(Expression.Property(Expression.Property(parameter, context.Metadata.ParentProperty.ClrName), typeof(T).GetProperty("Index")), Expression.Constant(parent));
+                if (equal == null)
+                    equal = item;
+                else
+                    equal = Expression.Or(equal, item);
+            }
+            var express = Expression.Lambda<Func<T, bool>>(equal, parameter);
+            return query.Where(express);
+
+        }
+    }
+}
--- src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/Entity/IEntityContext.cs
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/Entity/IEntityContext.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Wodsoft.ComBoost.Data.Entity.Metadata;
 
 namespace Wodsoft.ComBoost.Data.Entity
 {
     public interface IEntityContext<T>
         where T : class, new()
     {
+        IEntityMetadata Metadata { get; }
         IDatabaseContext Database { get; }
+
         void Add(T item);
         void AddRange(IEnumerable<T> items);
         T Create();
         void Update(T item);
+        void UpdateRange(IEnumerable<T> items);
         void Remove(T item);
         void RemoveRange(IEnumerable<T> items);
         IQueryable<T> Query();
-        Task<T> GetAsync(object key);
-        IOrderedQueryable<T> Order();
-        IOrderedQueryable<T> Order(IQueryable<T> query);
-        IQueryable<T> InParent(IQueryable<T> query, string path, object id);
-        IQueryable<T> InParent(IQueryable<T> query, object[] parentIds);
         Task<T[]> ToArrayAsync(IQueryable<T> query);
         Task<List<T>> ToListAsync(IQueryable<T> query);
-        bool IsNew(T item);
+        Task<T> SingleOrDefaultAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
+        Task<T> SingleAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
+        Task<T> FirstOrDefaultAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
+        Task<T> FirstAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
+        Task<T> LastOrDefaultAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
+        Task<T> LastAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
+        Task<int> CountAsync(IQueryable<T> query);
+        Task<int> CountAsync(IQueryable<T> query, Expression<Func<T, bool>> expression);
     }
 }
