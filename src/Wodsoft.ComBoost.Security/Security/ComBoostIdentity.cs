87b522a48ed712458da5c9364018949f4232dc3c
Kation L <kation@outlook.com>
2016-06-08 18:13:01 +0800

    更新权限

---
+++ src/Wodsoft.ComBoost.Security/Security/ComBoostIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost.Security
+{
+    public class ComBoostIdentity : ClaimsIdentity
+    {
+        public ComBoostIdentity()
+        {
+            
+        }
+    }
+}
---
+++ src/Wodsoft.ComBoost.Security/Security/ComBoostPrincipal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost.Security
+{
+    public class ComBoostPrincipal : System.Security.Claims.ClaimsPrincipal
+    {
+    }
+}
