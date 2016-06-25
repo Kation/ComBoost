7ab71acffea3780df830f02800c6955e89f0c4a6
Kation <lwcqq@163.com>
2016-06-25 13:21:07 +0800

    添加.Net 4.5.1支持

--- src/Wodsoft.ComBoost.Security/Security/ComBoostClaim.cs
+++ src/Wodsoft.ComBoost.Security/Security/ComBoostClaim.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Security.Claims;
+//using System.Threading.Tasks;
 
-namespace Wodsoft.ComBoost.Security
-{
-    public class ComBoostClaim : Claim
-    {
-        public ComBoostClaim(BinaryReader reader) : base(reader)
-        {
-            if (CustomSerializationData != null && CustomSerializationData.Length == 8)
-                ExpiredDate = new DateTime(BitConverter.ToInt64(CustomSerializationData, 0));
-        }
+//namespace Wodsoft.ComBoost.Security
+//{
+//    public class ComBoostClaim : Claim
+//    {
+//        public ComBoostClaim(BinaryReader reader) : base(reader)
+//        {
+//            if (CustomSerializationData != null && CustomSerializationData.Length == 8)
+//                ExpiredDate = new DateTime(BitConverter.ToInt64(CustomSerializationData, 0));
+//        }
 
-        public ComBoostClaim(string type, string value, DateTime expiredDate)
-            : base(type, value)
-        {
-            ExpiredDate = expiredDate;
-        }
+//        public ComBoostClaim(string type, string value, DateTime expiredDate)
+//            : base(type, value)
+//        {
+//            ExpiredDate = expiredDate;
+//        }
 
-        public DateTime ExpiredDate { get; private set; }
+//        public DateTime ExpiredDate { get; private set; }
 
-        public override void WriteTo(BinaryWriter writer)
-        {
-            base.WriteTo(writer, BitConverter.GetBytes(ExpiredDate.Ticks));
-        }
-    }
-}
+//        public override void WriteTo(BinaryWriter writer)
+//        {
+//            base.WriteTo(writer, BitConverter.GetBytes(ExpiredDate.Ticks));
+//        }
+//    }
+//}
