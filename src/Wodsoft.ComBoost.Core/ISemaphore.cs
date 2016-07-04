6b583913143964e6431a4f9808740f1334fb4aeb
Kation <Kation@KATION-WORK>
2016-07-04 18:09:49 +0800

    添加信号量提供器

---
+++ src/Wodsoft.ComBoost.Core/ISemaphore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost
+{
+    public interface ISemaphore : IDisposable
+    {
+        Task EnterAsync();
+
+        Task<bool> TryEnterAsync();
+
+        Task<bool> TryEnterAsync(int timeout);
+
+        Task<bool> EnterAsync(int timeout);
+
+        Task ExitAsync();
+    }
+}
---
+++ src/Wodsoft.ComBoost.Core/ISemaphoreProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost
+{
+    public interface ISemaphoreProvider
+    {
+        ISemaphore GetMutex(string name);
+
+        ISemaphore GetMonitor(string name);
+
+        ISemaphore GetSemaphore(string name, int count);
+    }
+}
