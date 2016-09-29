8cb96f3c3412d1e4ec092543d755ca590f2fb5e4
Kation <Kation@KATION-WORK>
2016-09-29 18:02:02 +0800

    修改odata支持

--- src/Wodsoft.ComBoost.Mvc.Data/EntityODataExtension.cs
+++ src/Wodsoft.ComBoost.Mvc.Data/EntityODataExtension.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wodsoft.ComBoost.Data;
 using Wodsoft.ComBoost.Data.Entity;
+using System.Linq.Expressions;
 
-namespace Wodsoft.ComBoost.Data
+namespace Wodsoft.ComBoost.Mvc
 {
     public class EntityODataExtension<T> : DomainExtension
         where T : class, IEntity, new()
@@ -21,14 +24,20 @@ namespace Wodsoft.ComBoost.Data
         private void Service_EntityQuery(IDomainExecutionContext context, EntityQueryEventArgs<T> e)
         {
 
-            var valueProvider = context.DomainContext.GetRequiredService<IValueProvider>();
-            int page = valueProvider.GetValue<int>("page");
-            if (page == 0)
-                page = 1;
-            int size = valueProvider.GetValue<int>("size");
-            if (size == 0)
-                size = EntityPagerExtension.DefaultPageSize;
-            e.Queryable = e.Queryable.Skip((page - 1) * size).Take(size);
+            //var valueProvider = context.DomainContext.GetRequiredService<IValueProvider>();
+            //int page = valueProvider.GetValue<int>("page");
+            //if (page == 0)
+            //    page = 1;
+            //int size = valueProvider.GetValue<int>("size");
+            //if (size == 0)
+            //    size = EntityPagerExtension.DefaultPageSize;
+            //e.Queryable = e.Queryable.Skip((page - 1) * size).Take(size);
+            var httpContext = context.DomainContext.GetRequiredService<HttpContextAccessor>().HttpContext;
+
+            Microsoft.OData.UriParser.ODataUriParser pathParser = new Microsoft.OData.UriParser.ODataUriParser(null, new Uri(httpContext.Request.Path));
+            var visitor = new ExpressionQueryNodeVisitor();
+            var expression = pathParser.ParseFilter().Expression.Accept(visitor);
+            e.Queryable = (IQueryable<T>)e.Queryable.Provider.CreateQuery(expression);
         }
     }
 }
