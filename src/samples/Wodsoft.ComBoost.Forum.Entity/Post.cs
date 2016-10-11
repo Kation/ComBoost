93a970d7b378e7886dd8d0c3c4f3a71bf018e169
Kation <kation@outlook.com>
2016-10-11 18:30:18 +0800

    更新

--- src/samples/Wodsoft.ComBoost.Forum.Entity/Post.cs
+++ src/samples/Wodsoft.ComBoost.Forum.Entity/Post.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wodsoft.ComBoost.Data.Entity;
+using Wodsoft.ComBoost.Forum.Core;
 
 namespace Wodsoft.ComBoost.Forum.Entity
 {
-    public class Post
+    public class Post : EntityBase, IPost
     {
+        public virtual string Content { get; set; }
+
+        public virtual Member Member { get; set; }
+
+        public virtual Thread Thread { get; set; }
+
+        IMember IPost.Member { get { return Member; } set { Member = (Member)value; } }
+
+        IThread IPost.Thread { get { return Thread; } set { Thread = (Thread)value; } }
     }
 }
