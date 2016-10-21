be24d56981df52ba9ad38beb7db90eab2eeb32dc
Kation <kation@outlook.com>
2016-10-21 18:22:40 +0800

    新增扩展方法

--- src/Wodsoft.ComBoost.Core/DomainServiceExtensions.cs
+++ src/Wodsoft.ComBoost.Core/DomainServiceExtensions.cs
@@ -111,107 +111,121 @@ namespace Wodsoft.ComBoost
             return domainService.ExecuteAsync(domainContext, method.GetMethodInfo());
         }
 
-        public static async Task<TResult> ExecuteAsync<TResult>(this IDomainService domainService, IDomainContext domainContext, Func<Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<TResult>(this IDomainService domainService, IDomainContext domainContext, Func<Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
         }
-        public static async Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, Task<TResult>> method)
+        public static Task<TResult> ExecuteAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(this IDomainService domainService, IDomainContext domainContext, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, Task<TResult>> method)
         {
             if (domainService == null)
                 throw new ArgumentNullException(nameof(domainService));
-            return await domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+            return domainService.ExecuteAsync<TResult>(domainContext, method.GetMethodInfo());
+        }
+
+        public static Task ExecuteAsync(this IDomainService domainService, IDomainContext domainContext, string method)
+        {
+            if (domainService == null)
+                throw new ArgumentNullException(nameof(domainService));
+            return domainService.ExecuteAsync(domainContext, domainService.GetType().GetMethod(method));
+        }
+
+        public static Task<TResult> ExecuteAsync<TResult>(this IDomainService domainService, IDomainContext domainContext, string method)
+        {
+            if (domainService == null)
+                throw new ArgumentNullException(nameof(domainService));
+            return domainService.ExecuteAsync<TResult>(domainContext, domainService.GetType().GetMethod(method));
         }
     }
 }
