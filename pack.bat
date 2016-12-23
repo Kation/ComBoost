3aa326c2772d546faab2d99a003db5464f056575
Kation <kation@outlook.com>
2016-12-23 10:37:52 +0800

    添加编译项目

--- pack.bat
+++ pack.bat
@@ -2,14 +2,16 @@
 cd build
 del *.nupkg
 cd..
-dotnet pack src\Wodsoft.ComBoost.Core --no-build --output build
-dotnet pack src\Wodsoft.ComBoost --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.Security --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.AspNetCore.Security --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.Data --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.Data.Core --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.EntityFramework --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.EntityFrameworkCore --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.Mvc --no-build --output build
-dotnet pack src\Wodsoft.ComBoost.Mvc.Data --no-build --output build
+dotnet pack src\Wodsoft.ComBoost.Core --output build
+dotnet pack src\Wodsoft.ComBoost --output build
+dotnet pack src\Wodsoft.ComBoost.Security --output build
+dotnet pack src\Wodsoft.ComBoost.AspNetCore.Security --output build
+dotnet pack src\Wodsoft.ComBoost.Data --output build
+dotnet pack src\Wodsoft.ComBoost.Data.Core --output build
+dotnet pack src\Wodsoft.ComBoost.EntityFramework --output build
+dotnet pack src\Wodsoft.ComBoost.EntityFrameworkCore --output build
+dotnet pack src\Wodsoft.ComBoost.Mvc --output build
+dotnet pack src\Wodsoft.ComBoost.Mvc.Data --output build
+dotnet pack src\Wodsoft.ComBoost.Storage --output build
+dotnet pack src\Wodsoft.ComBoost.Redis --output build
 pause
