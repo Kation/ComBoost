58750d82d8ae4015832efb18e993c63df9a12e73
Kation <kation@outlook.com>
2017-01-05 10:12:31 +0800

    允许不修改密码

--- src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/EntityPasswordExtension.cs
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/EntityPasswordExtension.cs
@@ -21,16 +21,16 @@ namespace Wodsoft.ComBoost.Data
         {
             if (e.Property.Type == System.ComponentModel.DataAnnotations.CustomDataType.Password)
             {
-                if ((string)e.Value == "")
-                    if (e.Entity.IsNewCreated)
-#if NET451
-            return Task.FromResult(0);
-#else
-                        return Task.CompletedTask;
-#endif
-                    else
-                        throw new ArgumentNullException("“" + e.Property.Name + "”不能为空。");
-                e.Entity.SetPassword((string)e.Value);
+                if (e.Value != null && (string)e.Value != "")
+                    //                    if (e.Entity.IsNewCreated)
+                    //#if NET451
+                    //            return Task.FromResult(0);
+                    //#else
+                    //                        return Task.CompletedTask;
+                    //#endif
+                    //                    else
+                    //                        throw new ArgumentNullException("“" + e.Property.Name + "”不能为空。");
+                    e.Entity.SetPassword((string)e.Value);
                 e.IsHandled = true;
             }
 #if NET451
