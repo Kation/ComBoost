6f6b96ccaaba68c17a250517277d176fa9f99024
Kation <kation@outlook.com>
2017-03-09 17:13:43 +0800

    修改为线程安全

--- src/Wodsoft.ComBoost/DomainProvider.cs
+++ src/Wodsoft.ComBoost/DomainProvider.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Wodsoft.ComBoost
 {
@@ -11,7 +12,7 @@ namespace Wodsoft.ComBoost
     {
         private IServiceProvider _ServiceProvider;
         private Dictionary<Type, Type> _Overrides;
-        private Dictionary<Type, IDomainService> _Cache;
+        private ConcurrentDictionary<Type, IDomainService> _Cache;
         private List<Func<Type, Type>> _ServiceSelectors, _ExtensionSelectors;
         private List<Func<Type, Type, bool>> _ExtensionFilters;
 
@@ -21,7 +22,7 @@ namespace Wodsoft.ComBoost
                 throw new ArgumentNullException(nameof(serviceProvider));
             _ServiceProvider = serviceProvider;
             _Overrides = new Dictionary<Type, Type>();
-            _Cache = new Dictionary<Type, IDomainService>();
+            _Cache = new ConcurrentDictionary<Type, IDomainService>();
             _ServiceSelectors = new List<Func<Type, Type>>();
             _ExtensionSelectors = new List<Func<Type, Type>>();
             _ExtensionFilters = new List<Func<Type, Type, bool>>();
@@ -30,31 +31,30 @@ namespace Wodsoft.ComBoost
         public virtual TService GetService<TService>() where TService : IDomainService
         {
             Type type = typeof(TService);
-            IDomainService service;
-            if (_Cache.TryGetValue(type, out service))
+            IDomainService service =
+            _Cache.GetOrAdd(type, ft =>
             {
-                return (TService)service;
-            }
-            if (_Overrides.ContainsKey(type))
-                type = _Overrides[type];
-            foreach (var selector in _ServiceSelectors)
-            {
-                type = selector(type);
-                if (type == null)
-                    throw new InvalidOperationException("领域服务选择器返回的类型为空。");
-            }
-            service = (TService)ActivatorUtilities.CreateInstance(_ServiceProvider, type);
-            Type[] extensions = _ExtensionSelectors.Select(t => t(type)).Where(t => t != null).ToArray();
-            foreach (var extensionType in extensions)
-            {
-                if (_ExtensionFilters.Any(t => t(type, extensionType) == false))
-                    continue;
-                IDomainExtension extension = (IDomainExtension)ActivatorUtilities.CreateInstance(_ServiceProvider, extensionType);
-                extension.OnInitialize(_ServiceProvider, service);
-                service.Executing += extension.OnExecutingAsync;
-                service.Executed += extension.OnExecutedAsync;
-            }
-            _Cache.Add(type, service);
+                if (_Overrides.ContainsKey(type))
+                    type = _Overrides[type];
+                foreach (var selector in _ServiceSelectors)
+                {
+                    type = selector(type);
+                    if (type == null)
+                        throw new InvalidOperationException("领域服务选择器返回的类型为空。");
+                }
+                service = (TService)ActivatorUtilities.CreateInstance(_ServiceProvider, type);
+                Type[] extensions = _ExtensionSelectors.Select(t => t(type)).Where(t => t != null).ToArray();
+                foreach (var extensionType in extensions)
+                {
+                    if (_ExtensionFilters.Any(t => t(type, extensionType) == false))
+                        continue;
+                    IDomainExtension extension = (IDomainExtension)ActivatorUtilities.CreateInstance(_ServiceProvider, extensionType);
+                    extension.OnInitialize(_ServiceProvider, service);
+                    service.Executing += extension.OnExecutingAsync;
+                    service.Executed += extension.OnExecutedAsync;
+                }
+                return service;
+            });
             return (TService)service;
         }
 
