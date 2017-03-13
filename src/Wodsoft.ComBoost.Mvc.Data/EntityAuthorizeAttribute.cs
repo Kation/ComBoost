7cd3300241e015b52a143c9baf9c6f28fd544315
Kation <kation@outlook.com>
2017-03-14 01:21:21 +0800

    修改Mvc筛选器

--- src/Wodsoft.ComBoost.Mvc.Data/EntityAuthorizeAttribute.cs
+++ src/Wodsoft.ComBoost.Mvc.Data/EntityAuthorizeAttribute.cs
@@ -6,94 +6,18 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using System.ComponentModel.DataAnnotations;
 using Wodsoft.ComBoost.Security;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace Wodsoft.ComBoost.Mvc
 {
-    public class EntityAuthorizeAttribute : Attribute, IActionFilter
+    public class EntityAuthorizeAttribute : ComBoostAuthorizeAttribute, IActionFilter
     {
-        /// <summary>
-        /// Initialize entity authorize attribute.
-        /// </summary>
-        /// <param name="action">Action of entity authorize.</param>
-        public EntityAuthorizeAttribute(EntityAuthorizeAction action)
+        protected override bool AuthorizeCore(FilterContext context, object controller)
         {
-            Action = action;
-        }
-
-        /// <summary>
-        /// Get the action of entity authorize.
-        /// </summary>
-        public EntityAuthorizeAction Action { get; private set; }
-
-        public virtual void OnActionExecuted(ActionExecutedContext context) { }
-
-        public virtual void OnActionExecuting(ActionExecutingContext context)
-        {
-            var controller = context.Controller as IHaveEntityMetadata;
+            var entityMetadata = controller as IHaveEntityMetadata;
             if (controller == null)
-                return;
-
-            bool isAuthed = true;
-            switch (Action)
-            {
-                case EntityAuthorizeAction.Create:
-                    isAuthed = controller.Metadata.AddRoles.Count() == 0 ||
-                        (controller.Metadata.AuthenticationRequiredMode == AuthenticationRequiredMode.All ?
-                        controller.Metadata.AddRoles.All(t => context.HttpContext.User.IsInDynamicRole(t)) :
-                        controller.Metadata.AddRoles.Any(t => context.HttpContext.User.IsInDynamicRole(t)));
-                    break;
-                case EntityAuthorizeAction.Edit:
-                    isAuthed = controller.Metadata.EditRoles.Count() == 0 ||
-                        (controller.Metadata.AuthenticationRequiredMode == AuthenticationRequiredMode.All ?
-                        controller.Metadata.EditRoles.All(t => context.HttpContext.User.IsInDynamicRole(t)) :
-                        controller.Metadata.EditRoles.Any(t => context.HttpContext.User.IsInDynamicRole(t)));
-                    break;
-                case EntityAuthorizeAction.Remove:
-                    isAuthed = controller.Metadata.RemoveRoles.Count() == 0 ||
-                        (controller.Metadata.AuthenticationRequiredMode == AuthenticationRequiredMode.All ?
-                        controller.Metadata.RemoveRoles.All(t => context.HttpContext.User.IsInDynamicRole(t)) :
-                        controller.Metadata.RemoveRoles.Any(t => context.HttpContext.User.IsInDynamicRole(t)));
-                    break;
-                case EntityAuthorizeAction.View:
-                    isAuthed = controller.Metadata.ViewRoles.Count() == 0 ||
-                        (controller.Metadata.AuthenticationRequiredMode == AuthenticationRequiredMode.All ?
-                        controller.Metadata.ViewRoles.All(t => context.HttpContext.User.IsInDynamicRole(t)) :
-                        controller.Metadata.ViewRoles.Any(t => context.HttpContext.User.IsInDynamicRole(t)));
-                    break;
-                default:
-                    isAuthed = false;
-                    break;
-            }
-            if (!isAuthed)
-            {
-                var middleware = context.HttpContext.Features.Get<ComBoostAuthenticationMiddleware>();
-                var loginUrl = middleware.Options.LoginPath(context.HttpContext);
-                loginUrl = context.HttpContext.Request.PathBase.Add(loginUrl + "?returlUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path));
-                context.Result = new RedirectResult(loginUrl);
-            }
-        }
-    }
-
-    /// <summary>
-    /// Entity authorize action
-    /// </summary>
-    public enum EntityAuthorizeAction
-    {
-        /// <summary>
-        /// View.
-        /// </summary>
-        View = 1,
-        /// <summary>
-        /// Create.
-        /// </summary>
-        Create = 2,
-        /// <summary>
-        /// Edit.
-        /// </summary>
-        Edit = 3,
-        /// <summary>
-        /// Remove
-        /// </summary>
-        Remove = 4
-    }
+                return true;
+            return entityMetadata.Metadata.AllowAnonymous || context.HttpContext.User.Identity.IsAuthenticated;
+        }        
+    }    
 }
--- src/Wodsoft.ComBoost.Mvc/ComBoostAuthorizeAttribute.cs
+++ src/Wodsoft.ComBoost.Mvc/ComBoostAuthorizeAttribute.cs
@@ -7,50 +7,42 @@ using Wodsoft.ComBoost.Security;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http.Features.Authentication;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
 
 namespace Wodsoft.ComBoost.Mvc
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class ComBoostAuthorizeAttribute : Attribute, IActionFilter
     {
-        public ComBoostAuthorizeAttribute()
-            : this(AuthenticationRequiredMode.All, new object[0])
-        { }
+        public ComBoostAuthorizeAttribute() { }
 
-        public ComBoostAuthorizeAttribute(params object[] roles) : this(AuthenticationRequiredMode.All, roles) { }
-
-        public ComBoostAuthorizeAttribute(AuthenticationRequiredMode mode, params object[] roles)
+        public virtual void OnActionExecuted(ActionExecutedContext context)
         {
-            Mode = mode;
-            Roles = roles;
+            if (context.Result is StatusCodeResult && ((StatusCodeResult)context.Result).StatusCode == 401 && !context.HttpContext.User.Identity.IsAuthenticated)
+                context.Result = GetLoginUrl(context);
+            else if (context.Result is ObjectResult && ((ObjectResult)context.Result).StatusCode == 401 && !context.HttpContext.User.Identity.IsAuthenticated)
+                context.Result = GetLoginUrl(context);
         }
-
-        public AuthenticationRequiredMode Mode { get; private set; }
-
-        public object[] Roles { get; private set; }
-
-        public virtual void OnActionExecuted(ActionExecutedContext context) { }
 
         public virtual void OnActionExecuting(ActionExecutingContext context)
         {
-            bool isAuthed = true;
-            if (!context.HttpContext.User.Identity.IsAuthenticated)
-            {
-                isAuthed = false;
-            }
-            else if (Mode == AuthenticationRequiredMode.All ?
-                !Roles.All(t => context.HttpContext.User.IsInDynamicRole(t)) :
-                !Roles.Any(t => context.HttpContext.User.IsInDynamicRole(t)))
-            {
-                isAuthed = false;
-            }
-            if (!isAuthed)
-            {
-                var options = context.HttpContext.RequestServices.GetRequiredService<ComBoostAuthenticationOptions>();
-                var loginUrl = options.LoginPath(context.HttpContext);
-                loginUrl = context.HttpContext.Request.PathBase.Add(loginUrl) + "?returlUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path);
-                context.Result = new RedirectResult(loginUrl);
-            }
+            if (context.Filters.Count(t => t is ComBoostAnonymousFilter) > 0)
+                return;
+            if (!AuthorizeCore(context, context.Controller))
+                context.Result = GetLoginUrl(context);
+        }
+
+        protected virtual bool AuthorizeCore(FilterContext context, object controller)
+        {
+            return context.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        protected virtual RedirectResult GetLoginUrl(FilterContext context)
+        {
+            var options = context.HttpContext.RequestServices.GetRequiredService<ComBoostAuthenticationOptions>();
+            var loginUrl = options.LoginPath(context.HttpContext);
+            loginUrl = context.HttpContext.Request.PathBase.Add(loginUrl) + "?returlUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path);
+            return new RedirectResult(loginUrl);
         }
     }
 }
