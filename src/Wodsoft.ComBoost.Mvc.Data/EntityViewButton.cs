9b0f6c36291c7791702e138d395769a361dc3d9b
Kation <kation@outlook.com>
2017-03-14 16:39:32 +0800

    修改Mvc列表按钮

--- src/Wodsoft.ComBoost.Mvc.Data/EntityViewButton.cs
+++ src/Wodsoft.ComBoost.Mvc.Data/EntityViewButton.cs
@@ -13,38 +13,13 @@ namespace Wodsoft.ComBoost.Mvc
     /// Mvc item button.
     /// Use for mvc viewlist page.
     /// </summary>
-    public class EntityItemButton : IEntityViewButton
+    public class EntityViewButton : ViewButton, IEntityViewButton
     {
-        /// <summary>
-        /// Get or set the name of button.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Get or set the icon url.
-        /// </summary>
-        public string Icon { get; set; }
-
-        /// <summary>
-        /// Get or set the tooltip.
-        /// </summary>
-        public string Tooltip { get; set; }
-
-        /// <summary>
-        /// Get or set the button link.
-        /// If value is not null, the button will be disabled.
-        /// </summary>
-        public string Link { get; set; }
-
         /// <summary>
         /// Get or set the link delegate.
         /// </summary>
-        public MvcEntityViewButtonLinkDelegate GetLink { get; set; }
-
-        object IViewButton.Icon { get { return Icon; } }
-
-        object IViewButton.Tooltip { get { return Tooltip; } }
-
+        public MvcEntityItemButtonLinkDelegate GetLink { get; set; }
+        
         /// <summary>
         /// Set the target of button.
         /// </summary>
@@ -58,14 +33,9 @@ namespace Wodsoft.ComBoost.Mvc
                 throw new NotSupportedException();
             var accessor = provider.GetService<Microsoft.AspNetCore.Mvc.Infrastructure.IActionContextAccessor>();
             var urlHelper = provider.GetService<Microsoft.AspNetCore.Mvc.Routing.IUrlHelperFactory>().GetUrlHelper(accessor.ActionContext);
-            Link = GetLink(urlHelper, entity);
+            Target = GetLink(urlHelper, entity);
         }
-
-        /// <summary>
-        /// Get the target of the button.
-        /// </summary>
-        public object Target { get { return Link; } }
-
+        
         void IViewButton.SetTarget(IServiceProvider provider)
         {
             throw new NotSupportedException();
@@ -78,5 +48,5 @@ namespace Wodsoft.ComBoost.Mvc
     /// <param name="url">Mvc url helper.</param>
     /// <param name="entity">Dependency entity.</param>
     /// <returns>Return url link.</returns>
-    public delegate string MvcEntityViewButtonLinkDelegate(IUrlHelper url, IEntity entity);
+    public delegate string MvcEntityItemButtonLinkDelegate(IUrlHelper url, IEntity entity);
 }
---
+++ src/Wodsoft.ComBoost.Mvc.Data/ViewButton.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wodsoft.ComBoost.Mvc
+{
+    public class ViewButton : IViewButton
+    {
+        /// <summary>
+        /// Get or set the name of button.
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// Get or set the icon url.
+        /// </summary>
+        public virtual string Icon { get; set; }
+
+        /// <summary>
+        /// Get or set the tooltip.
+        /// </summary>
+        public virtual string Tooltip { get; set; }
+
+        object IViewButton.Icon { get { return Icon; } }
+
+        object IViewButton.Tooltip { get { return Tooltip; } }
+        
+        /// <summary>
+        /// Get or set the target of the button.
+        /// </summary>
+        public virtual object Target { get; set; }
+
+        public void SetTarget(IServiceProvider provider)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public delegate string MvcEntityViewButtonLinkDelegate(IUrlHelper url);
+}
