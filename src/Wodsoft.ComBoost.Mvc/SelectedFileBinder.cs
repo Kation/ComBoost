dbc4da7736ea4e9f9a7cab12bb6915b0776d9ffb
Kation <kation@outlook.com>
2017-05-18 16:06:41 +0800

    去除重复

--- src/Wodsoft.ComBoost.Mvc/SelectedFileBinder.cs
+++ src/Wodsoft.ComBoost.Mvc/SelectedFileBinder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Wodsoft.ComBoost.AspNetCore;
 
 namespace Wodsoft.ComBoost.Mvc
 {
