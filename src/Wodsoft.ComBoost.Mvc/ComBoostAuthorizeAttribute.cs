459d48a86141850148b76eca48f240d0eb30b920
农志升 <nongzhsh@outlook.com>
2017-09-27 12:06:15 +0800

    修正：returlUrl->returnUrl

--- src/Wodsoft.ComBoost.Mvc/ComBoostAuthorizeAttribute.cs
+++ src/Wodsoft.ComBoost.Mvc/ComBoostAuthorizeAttribute.cs
@@ -44,9 +44,9 @@ namespace Wodsoft.ComBoost.Mvc
             var loginUrl = options.LoginPath(context.HttpContext);
             loginUrl = context.HttpContext.Request.PathBase.Add(loginUrl);
             if (loginUrl.Contains("?"))
-                loginUrl += "&returlUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path);
+                loginUrl += "&returnUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path);
             else
-                loginUrl += "?returlUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path);
+                loginUrl += "?returnUrl=" + Uri.EscapeDataString(context.HttpContext.Request.Path);
             return new RedirectResult(loginUrl);
         }
     }
