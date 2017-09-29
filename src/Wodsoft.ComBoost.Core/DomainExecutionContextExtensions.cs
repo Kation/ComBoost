6c13e0ef0bae3dc22c8b7a3cce90cf3f2a115e26
Kation <kation@outlook.com>
2017-09-29 10:07:05 +0800

    添加从执行上下文获取领域服务的扩展方法

---
+++ src/Wodsoft.ComBoost.Core/DomainExecutionContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wodsoft.ComBoost
+{
+    /// <summary>
+    /// 领域执行上下文扩展。
+    /// </summary>
+    public static class DomainExecutionContextExtensions
+    {
+        /// <summary>
+        /// 获取领域服务。
+        /// </summary>
+        /// <typeparam name="T">领域服务类型。</typeparam>
+        /// <param name="context">当前领域执行上下文。</param>
+        /// <returns>获取到的领域服务。</returns>
+        public static T GetDomainService<T>(this IDomainExecutionContext context)
+            where T : IDomainService
+        {
+            var domainProvider = context.DomainContext.GetRequiredService<IDomainServiceProvider>();
+            return domainProvider.GetService<T>();
+        }
+    }
+}
