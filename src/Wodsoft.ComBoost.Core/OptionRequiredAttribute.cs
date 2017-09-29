58df663321719b10e06c7171d07040e538ecbcc9
Kation <kation@outlook.com>
2017-09-29 10:14:09 +0800

    添加需要特性

---
+++ src/Wodsoft.ComBoost.Core/OptionRequiredAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wodsoft.ComBoost
+{
+    /// <summary>
+    /// 用于标注领域方法需要的选项。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class OptionRequiredAttribute : Attribute
+    {
+        /// <summary>
+        /// 领域方法需要的服务类型。
+        /// </summary>
+        /// <param name="type"></param>
+        public OptionRequiredAttribute(Type type)
+        {
+            Type = type;
+        }
+        
+        /// <summary>
+        /// 获取值类型。
+        /// </summary>
+        public Type Type { get; private set; }
+    }
+}
---
+++ src/Wodsoft.ComBoost.Core/ValueRequiredAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wodsoft.ComBoost
+{
+    /// <summary>
+    /// 用于标注领域方法需要的值。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class ValueRequiredAttribute : Attribute
+    {
+        /// <summary>
+        /// 领域方法需要的值名称。
+        /// </summary>
+        /// <param name="name">值名称。</param>
+        public ValueRequiredAttribute(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// 领域方法需要的值名称与类型。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="name">值名称。</param>
+        public ValueRequiredAttribute(Type type, string name)
+            : this(name)
+        {
+            Type = type;
+        }
+
+        /// <summary>
+        /// 获取值名称。
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// 获取值类型。
+        /// </summary>
+        public Type Type { get; private set; }
+    }
+}
