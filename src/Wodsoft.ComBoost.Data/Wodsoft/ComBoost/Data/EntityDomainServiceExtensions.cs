060d1d78f6d02bd3a1ef05c0f980f73b17f4502d
Kation <kation@outlook.com>
2017-09-30 17:30:37 +0800

    添加便捷扩展方法

--- src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/EntityDomainServiceExtensions.cs
+++ src/Wodsoft.ComBoost.Data/Wodsoft/ComBoost/Data/EntityDomainServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,32 @@ using Wodsoft.ComBoost.Security;
 
 namespace Wodsoft.ComBoost.Data
 {
+    /// <summary>
+    /// 实体领域服务扩展方法。
+    /// </summary>
     public static class EntityDomainServiceExtensions
     {
+        /// <summary>
+        /// 执行列表。
+        /// </summary>
+        /// <typeparam name="T">实体类型。</typeparam>
+        /// <param name="domain">实体领域服务。</param>
+        /// <param name="context">领域上下文。</param>
+        /// <returns>返回实体视图模型。</returns>
         public static Task<IEntityViewModel<T>> ExecuteList<T>(this EntityDomainService<T> domain, IDomainContext context)
             where T : class, IEntity, new()
         {
+            return domain.ExecuteAsync<IDatabaseContext, IAuthenticationProvider, EntityDomainAuthorizeOption, IEntityViewModel<T>>(context, domain.List);
+        }
+
+        public static Task<IEntityViewModel<T>> ExecuteList<T>(this EntityDomainService<T> domain, IDomainContext context, Expression<Func<T, bool>> whereExpression)
+            where T : class, IEntity, new()
+        {
+            context.EventManager.AddAsyncEventHandler<EntityQueryEventArgs<T>>(EntityDomainService<T>.EntityQueryEvent, (d, e) =>
+            {
+                e.Queryable = e.Queryable.Where(whereExpression);
+                return Task.CompletedTask;
+            });
             return domain.ExecuteAsync<IDatabaseContext, IAuthenticationProvider, EntityDomainAuthorizeOption, IEntityViewModel<T>>(context, domain.List);
         }
 
@@ -22,6 +44,19 @@ namespace Wodsoft.ComBoost.Data
             where T : class, IEntity, new()
             where TModel : class
         {
+            context.Options.SetOption(new EntityQuerySelectOption<T, TModel>(selectExpression));
+            return domain.ExecuteAsync<IDatabaseContext, IAuthenticationProvider, EntityDomainAuthorizeOption, EntityQuerySelectOption<T, TModel>, IViewModel<TModel>>(context, domain.ListViewModel);
+        }
+
+        public static Task<IViewModel<TModel>> ExecuteListViewModel<T, TModel>(this EntityDomainService<T> domain, IDomainContext context, Expression<Func<T, bool>> whereExpression, Expression<Func<T, TModel>> selectExpression)
+            where T : class, IEntity, new()
+            where TModel : class
+        {
+            context.EventManager.AddAsyncEventHandler<EntityQueryEventArgs<T>>(EntityDomainService<T>.EntityQueryEvent, (d, e) =>
+            {
+                e.Queryable = e.Queryable.Where(whereExpression);
+                return Task.CompletedTask;
+            });
             context.Options.SetOption(new EntityQuerySelectOption<T, TModel>(selectExpression));
             return domain.ExecuteAsync<IDatabaseContext, IAuthenticationProvider, EntityDomainAuthorizeOption, EntityQuerySelectOption<T, TModel>, IViewModel<TModel>>(context, domain.ListViewModel);
         }
