d43fa6f5fb9d6e1cde449f2e1b9a8ff8c1a8049a
Kation <kation@outlook.com>
2018-03-15 10:52:27 +0800

    修复可空值时异常的问题

--- src/Wodsoft.ComBoost.Data.Core/Wodsoft/ComBoost/FromEntityAttribute.cs
+++ src/Wodsoft.ComBoost.Data.Core/Wodsoft/ComBoost/FromEntityAttribute.cs
@@ -9,32 +9,55 @@ using Wodsoft.ComBoost.Data.Entity.Metadata;
 
 namespace Wodsoft.ComBoost
 {
+    /// <summary>
+    /// 实体来源特性。
+    /// </summary>
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
     public class FromEntityAttribute : FromAttribute
     {
+        /// <summary>
+        /// 实例化实体来源特性。默认必须值。
+        /// </summary>
         public FromEntityAttribute() { IsRequired = true; }
 
+        /// <summary>
+        /// 实例化实体来源特性。
+        /// </summary>
+        /// <param name="isRequired">是否必须。</param>
         public FromEntityAttribute(bool isRequired) { IsRequired = isRequired; }
 
         /// <summary>
-        /// 自定义来源名称。
+        /// 获取自定义来源名称。
         /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// 获取是否为必须。
+        /// </summary>
         public bool IsRequired { get; private set; }
 
+        /// <summary>
+        /// 获取值。
+        /// </summary>
+        /// <param name="executionContext">领域执行上下文。</param>
+        /// <param name="parameter">参数信息。</param>
+        /// <returns>返回值。</returns>
         public override object GetValue(IDomainExecutionContext executionContext, ParameterInfo parameter)
         {
+            var metadata = EntityDescriptor.GetMetadata(parameter.ParameterType);
             IValueProvider provider = executionContext.DomainContext.GetRequiredService<IValueProvider>();
-            object value = provider.GetValue(Name ?? parameter.Name, EntityDescriptor.GetMetadata(parameter.ParameterType).KeyType);
+            var keyType = metadata.KeyType;
+            if (keyType.GetTypeInfo().IsValueType)
+                keyType = typeof(Nullable<>).MakeGenericType(keyType);
+            object value = provider.GetValue(Name ?? parameter.Name, keyType);
             if (value == null)
-                throw new DomainServiceException(new ArgumentNullException(parameter.Name, "获取" + (Name ?? parameter.Name) + "实体的值为空。"));
+                if (IsRequired)
+                    throw new DomainServiceException(new ArgumentNullException(parameter.Name, "获取" + (Name ?? parameter.Name) + "实体的值为空。"));
+                else
+                    return null;
             var databaseContext = executionContext.DomainContext.GetRequiredService<IDatabaseContext>();
             dynamic entityContext;
-            //if (parameter.ParameterType.GetTypeInfo().IsInterface)
-            //    entityContext = typeof(DatabaseContextExtensions).GetMethod("GetWrappedContext").MakeGenericMethod(parameter.ParameterType).Invoke(null, new object[] { databaseContext });
-            //else
-            var type = EntityDescriptor.GetMetadata(parameter.ParameterType).Type;
+            var type = metadata.Type;
             entityContext = typeof(IDatabaseContext).GetMethod("GetContext").MakeGenericMethod(type).Invoke(databaseContext, new object[0]);
             object entity = entityContext.GetAsync(value).Result;
             if (IsRequired && entity == null)
