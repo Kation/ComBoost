b2e987994a47dd3f721f7cc428611dfb1b8d9ce2
Kation <kation@outlook.com>
2018-03-15 11:00:41 +0800

    增加控制器属性

--- src/Wodsoft.ComBoost.Mvc/ControllerDomainContext.cs
+++ src/Wodsoft.ComBoost.Mvc/ControllerDomainContext.cs
@@ -6,10 +6,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Wodsoft.ComBoost.Mvc
 {
+    /// <summary>
+    /// 控制器领域上下文。
+    /// </summary>
     public class ControllerDomainContext : MvcDomainContext
     {
+        /// <summary>
+        /// 实例化控制器领域上下文。
+        /// </summary>
+        /// <param name="controller">控制器。</param>
         public ControllerDomainContext(Controller controller) : base(controller.ControllerContext)
         {
-        }        
+            Controller = controller;
+        }
+
+        /// <summary>
+        /// 获取控制器。
+        /// </summary>
+        public Controller Controller { get; private set; }
     }
 }
