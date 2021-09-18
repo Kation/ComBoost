4c45cbffd81899be1faa8061422b10fde33d29d2
Kation <kation@outlook.com>
2021-09-18 11:26:35 +0800

    Fix response serialization bug

--- src/Wodsoft.ComBoost.Grpc/DomainGrpcResponse.cs
+++ src/Wodsoft.ComBoost.Grpc/DomainGrpcResponse.cs
@@ -9,6 +9,7 @@ using System.Reflection.Emit;
 using System.Text;
 using Wodsoft.Protobuf;
 using Wodsoft.Protobuf.Generators;
+using Wodsoft.Protobuf.Primitives;
 
 namespace Wodsoft.ComBoost.Grpc
 {
@@ -98,7 +99,11 @@ namespace Wodsoft.ComBoost.Grpc
         protected override int CalculateSize()
         {
             var size = base.CalculateSize();
-            size += _CalculateResultSize(this, ref _result);
+            if (Result != null)
+            {
+                Message<T> message = Result;
+                size += ((IMessage)message).CalculateSize() + 1;
+            }
             return size;
         }
 
@@ -106,7 +111,9 @@ namespace Wodsoft.ComBoost.Grpc
         {
             if (tag == _ResultTag)
             {
-                Result = _ReadResult(this, ref parser);
+                Message<T> message = Message.New<T>();
+                ((IBufferMessage)message).InternalMergeFrom(ref parser);
+                Result = message;
             }
         }
 
@@ -114,183 +121,196 @@ namespace Wodsoft.ComBoost.Grpc
         {
             base.Write(ref writer);
             if (Result != null)
-                _WriteResult(this, ref writer, _result);
+            {
+                writer.WriteRawTag(_ResultTag);
+                Message<T> message = Result;
+                ((IBufferMessage)message).InternalWriteTo(ref writer);
+            }
         }
 
-        private static readonly uint _ResultTag;
-        private static readonly CalculateResultSize _CalculateResultSize;
-        private static readonly ParseResult _ReadResult;
-        private static readonly WriteResult _WriteResult;
-        private delegate int CalculateResultSize(DomainGrpcResponse<T> source, ref T result);
-        private delegate void WriteResult(DomainGrpcResponse<T> source, ref WriteContext writer, T result);
-        private delegate T ParseResult(DomainGrpcResponse<T> source, ref ParseContext parser);
-
+        private static readonly byte _ResultTag;
         static DomainGrpcResponse()
         {
-            var resultType = typeof(T);
             var codeGenerator = MessageBuilder.GetCodeGenerator<T>();
-
-            DynamicMethod calculateSize = new DynamicMethod("CalculateResultSize", typeof(int), new Type[] { typeof(DomainGrpcResponse<T>), resultType.MakeByRefType() });
-            var calculateSizeILGenerator = calculateSize.GetILGenerator();
-            var calculateSizeEnd = calculateSizeILGenerator.DefineLabel();
-
-            DynamicMethod write = new DynamicMethod("WriteResult", null, new Type[] { typeof(DomainGrpcResponse<T>), typeof(WriteContext).MakeByRefType(), resultType });
-            var writeILGenerator = write.GetILGenerator();
-            var writeEnd = writeILGenerator.DefineLabel();
-
-            DynamicMethod read = new DynamicMethod("ReadResult", resultType, new Type[] { typeof(DomainGrpcResponse<T>), typeof(ParseContext).MakeByRefType() });
-            var readILGenerator = read.GetILGenerator();
-
-            //CalculateSize
-            var calculateSizeValueVariable = calculateSizeILGenerator.DeclareLocal(resultType.MakeByRefType());
-            calculateSizeILGenerator.Emit(OpCodes.Ldarg_1);
-            calculateSizeILGenerator.Emit(OpCodes.Stloc, calculateSizeValueVariable);
-            //Write
-            var writeValueVariable = writeILGenerator.DeclareLocal(resultType.MakeByRefType());
-            writeILGenerator.Emit(OpCodes.Ldarg_2);
-            writeILGenerator.Emit(OpCodes.Stloc, writeValueVariable);
-
-
             if (codeGenerator == null)
-            {
-                if (!resultType.IsClass)
-                    throw new NotSupportedException($"Do not support response type \"{resultType.FullName}\".");
-                if (resultType.GetConstructor(Array.Empty<Type>()) == null)
-                    throw new NotSupportedException($"Response type \"{resultType.FullName}\" doesn't have a default constructor.");
-                codeGenerator = (ICodeGenerator<T>)Activator.CreateInstance(typeof(ObjectCodeGenerator<>).MakeGenericType(resultType));
-            }
-            _ResultTag = WireFormat.MakeTag(5, codeGenerator.WireType);
-            //CalculateSize
-            {
-                codeGenerator.GenerateCalculateSizeCode(calculateSizeILGenerator, calculateSizeValueVariable, 5);
-            }
-            //Read
-            {
-                codeGenerator.GenerateReadCode(readILGenerator);
-            }
-            //Write
-            {
-                codeGenerator.GenerateWriteCode(writeILGenerator, writeValueVariable, 5);
-            }
-            calculateSizeILGenerator.MarkLabel(calculateSizeEnd);
-            calculateSizeILGenerator.Emit(OpCodes.Ret);
-            readILGenerator.Emit(OpCodes.Ret);
-            writeILGenerator.MarkLabel(writeEnd);
-            writeILGenerator.Emit(OpCodes.Ret);
-
-            _CalculateResultSize = (CalculateResultSize)calculateSize.CreateDelegate(typeof(CalculateResultSize));
-            _ReadResult = (ParseResult)read.CreateDelegate(typeof(ParseResult));
-            _WriteResult = (WriteResult)write.CreateDelegate(typeof(WriteResult));
+                _ResultTag = (byte)WireFormat.MakeTag(5, WireFormat.WireType.LengthDelimited);
+            else
+                _ResultTag = (byte)WireFormat.MakeTag(5, codeGenerator.WireType);
         }
+
+        //private static readonly CalculateResultSize _CalculateResultSize;
+        //private static readonly ParseResult _ReadResult;
+        //private static readonly WriteResult _WriteResult;
+        //private delegate int CalculateResultSize(DomainGrpcResponse<T> source, ref T result);
+        //private delegate void WriteResult(DomainGrpcResponse<T> source, ref WriteContext writer, T result);
+        //private delegate T ParseResult(DomainGrpcResponse<T> source, ref ParseContext parser);
+
+        //static DomainGrpcResponse()
+        //{
+        //    var resultType = typeof(T);
+        //    var codeGenerator = MessageBuilder.GetCodeGenerator<T>();
+
+        //    DynamicMethod calculateSize = new DynamicMethod("CalculateResultSize", typeof(int), new Type[] { typeof(DomainGrpcResponse<T>), resultType.MakeByRefType() });
+        //    var calculateSizeILGenerator = calculateSize.GetILGenerator();
+        //    var calculateSizeEnd = calculateSizeILGenerator.DefineLabel();
+
+        //    DynamicMethod write = new DynamicMethod("WriteResult", null, new Type[] { typeof(DomainGrpcResponse<T>), typeof(WriteContext).MakeByRefType(), resultType });
+        //    var writeILGenerator = write.GetILGenerator();
+        //    var writeEnd = writeILGenerator.DefineLabel();
+
+        //    DynamicMethod read = new DynamicMethod("ReadResult", resultType, new Type[] { typeof(DomainGrpcResponse<T>), typeof(ParseContext).MakeByRefType() });
+        //    var readILGenerator = read.GetILGenerator();
+
+        //    //CalculateSize
+        //    var calculateSizeValueVariable = calculateSizeILGenerator.DeclareLocal(resultType.MakeByRefType());
+        //    calculateSizeILGenerator.Emit(OpCodes.Ldarg_1);
+        //    calculateSizeILGenerator.Emit(OpCodes.Stloc, calculateSizeValueVariable);
+        //    //Write
+        //    var writeValueVariable = writeILGenerator.DeclareLocal(resultType.MakeByRefType());
+        //    writeILGenerator.Emit(OpCodes.Ldarg_2);
+        //    writeILGenerator.Emit(OpCodes.Stloc, writeValueVariable);
+
+
+        //    if (codeGenerator == null)
+        //    {
+        //        if (!resultType.IsClass)
+        //            throw new NotSupportedException($"Do not support response type \"{resultType.FullName}\".");
+        //        if (resultType.GetConstructor(Array.Empty<Type>()) == null)
+        //            throw new NotSupportedException($"Response type \"{resultType.FullName}\" doesn't have a default constructor.");
+        //        codeGenerator = (ICodeGenerator<T>)Activator.CreateInstance(typeof(ObjectCodeGenerator<>).MakeGenericType(resultType));
+        //    }
+        //    _ResultTag = WireFormat.MakeTag(5, codeGenerator.WireType);
+        //    //CalculateSize
+        //    {
+        //        codeGenerator.GenerateCalculateSizeCode(calculateSizeILGenerator, calculateSizeValueVariable, 5);
+        //    }
+        //    //Read
+        //    {
+        //        codeGenerator.GenerateReadCode(readILGenerator);
+        //    }
+        //    //Write
+        //    {
+        //        codeGenerator.GenerateWriteCode(writeILGenerator, writeValueVariable, 5);
+        //    }
+        //    calculateSizeILGenerator.MarkLabel(calculateSizeEnd);
+        //    calculateSizeILGenerator.Emit(OpCodes.Ret);
+        //    readILGenerator.Emit(OpCodes.Ret);
+        //    writeILGenerator.MarkLabel(writeEnd);
+        //    writeILGenerator.Emit(OpCodes.Ret);
+
+        //    _CalculateResultSize = (CalculateResultSize)calculateSize.CreateDelegate(typeof(CalculateResultSize));
+        //    _ReadResult = (ParseResult)read.CreateDelegate(typeof(ParseResult));
+        //    _WriteResult = (WriteResult)write.CreateDelegate(typeof(WriteResult));
+        //}
     }
 
-    public class DomainGrpcCollectionResponse<TContainer, TElement> : DomainGrpcResponse, IDomainRpcResponse<TContainer>
-        where TContainer : class, ICollection<TElement>
-        where TElement : new()
-    {
-        private TContainer _result;
-        public TContainer Result { get => _result; set => _result = value; }
+    //public class DomainGrpcCollectionResponse<TContainer, TElement> : DomainGrpcResponse, IDomainRpcResponse<TContainer>
+    //    where TContainer : class, ICollection<TElement>
+    //    where TElement : new()
+    //{
+    //    private TContainer _result;
+    //    public TContainer Result { get => _result; set => _result = value; }
 
-        private RepeatedField<TElement> _container;
+    //    private RepeatedField<TElement> _container;
 
-        protected override int CalculateSize()
-        {
-            var size = base.CalculateSize();
-            var container = new RepeatedField<TElement>();
-            container.AddRange(Result);
-            size += container.CalculateSize(_ResultCodec);
-            return size;
-        }
+    //    protected override int CalculateSize()
+    //    {
+    //        var size = base.CalculateSize();
+    //        var container = new RepeatedField<TElement>();
+    //        container.AddRange(Result);
+    //        size += container.CalculateSize(_ResultCodec);
+    //        return size;
+    //    }
 
-        protected override void Read(ref ParseContext parser)
-        {
-            _container = new RepeatedField<TElement>();
-            base.Read(ref parser);
-        }
+    //    protected override void Read(ref ParseContext parser)
+    //    {
+    //        _container = new RepeatedField<TElement>();
+    //        base.Read(ref parser);
+    //    }
 
-        protected override void ReadTag(uint tag, ref ParseContext parser)
-        {
-            if (tag == _ResultTag1 || tag == _ResultTag2)
-            {
-                _container.AddEntriesFrom(ref parser, _ResultCodec);
-            }
-        }
+    //    protected override void ReadTag(uint tag, ref ParseContext parser)
+    //    {
+    //        if (tag == _ResultTag1 || tag == _ResultTag2)
+    //        {
+    //            _container.AddEntriesFrom(ref parser, _ResultCodec);
+    //        }
+    //    }
 
-        protected override void Write(ref WriteContext writer)
-        {
-            base.Write(ref writer);
-            var container = new RepeatedField<TElement>();
-            container.AddRange(Result);
-            container.WriteTo(ref writer, _ResultCodec);
-        }
+    //    protected override void Write(ref WriteContext writer)
+    //    {
+    //        base.Write(ref writer);
+    //        var container = new RepeatedField<TElement>();
+    //        container.AddRange(Result);
+    //        container.WriteTo(ref writer, _ResultCodec);
+    //    }
 
-        private static readonly uint _ResultTag1, _ResultTag2;
-        private static FieldCodec<TElement> _ResultCodec;
-        static DomainGrpcCollectionResponse()
-        {
-            _ResultTag1 = WireFormat.MakeTag(4, WireFormat.WireType.LengthDelimited);
-            var codeGenerator = MessageBuilder.GetCodeGenerator<TElement>();
-            if (codeGenerator == null)
-                codeGenerator = new ObjectCodeGenerator<TElement>();
-            if (codeGenerator.WireType == WireFormat.WireType.Varint)
-                _ResultTag2 = WireFormat.MakeTag(4, WireFormat.WireType.Varint);
-            _ResultCodec = codeGenerator.CreateFieldCodec(5);
-        }
-    }
+    //    private static readonly uint _ResultTag1, _ResultTag2;
+    //    private static FieldCodec<TElement> _ResultCodec;
+    //    static DomainGrpcCollectionResponse()
+    //    {
+    //        _ResultTag1 = WireFormat.MakeTag(4, WireFormat.WireType.LengthDelimited);
+    //        var codeGenerator = MessageBuilder.GetCodeGenerator<TElement>();
+    //        if (codeGenerator == null)
+    //            codeGenerator = new ObjectCodeGenerator<TElement>();
+    //        if (codeGenerator.WireType == WireFormat.WireType.Varint)
+    //            _ResultTag2 = WireFormat.MakeTag(4, WireFormat.WireType.Varint);
+    //        _ResultCodec = codeGenerator.CreateFieldCodec(5);
+    //    }
+    //}
 
-    public class DomainGrpcDictionaryResponse<TContainer, TKey, TValue> : DomainGrpcResponse, IDomainRpcResponse<TContainer>
-        where TContainer : class, IDictionary<TKey, TValue>
-        where TKey : new()
-        where TValue : new()
-    {
-        private TContainer _result;
-        public TContainer Result { get => _result; set => _result = value; }
+    //public class DomainGrpcDictionaryResponse<TContainer, TKey, TValue> : DomainGrpcResponse, IDomainRpcResponse<TContainer>
+    //    where TContainer : class, IDictionary<TKey, TValue>
+    //    where TKey : new()
+    //    where TValue : new()
+    //{
+    //    private TContainer _result;
+    //    public TContainer Result { get => _result; set => _result = value; }
 
-        private MapField<TKey, TValue> _container;
+    //    private MapField<TKey, TValue> _container;
 
-        protected override int CalculateSize()
-        {
-            var size = base.CalculateSize();
-            var container = new MapField<TKey, TValue>();
-            container.Add(Result);
-            size += container.CalculateSize(_ResultCodec);
-            return size;
-        }
+    //    protected override int CalculateSize()
+    //    {
+    //        var size = base.CalculateSize();
+    //        var container = new MapField<TKey, TValue>();
+    //        container.Add(Result);
+    //        size += container.CalculateSize(_ResultCodec);
+    //        return size;
+    //    }
 
-        protected override void Read(ref ParseContext parser)
-        {
-            _container = new MapField<TKey, TValue>();
-            base.Read(ref parser);
-        }
+    //    protected override void Read(ref ParseContext parser)
+    //    {
+    //        _container = new MapField<TKey, TValue>();
+    //        base.Read(ref parser);
+    //    }
 
-        protected override void ReadTag(uint tag, ref ParseContext parser)
-        {
-            if (tag == _ResultTag)
-            {
-                _container.AddEntriesFrom(ref parser, _ResultCodec);
-            }
-        }
+    //    protected override void ReadTag(uint tag, ref ParseContext parser)
+    //    {
+    //        if (tag == _ResultTag)
+    //        {
+    //            _container.AddEntriesFrom(ref parser, _ResultCodec);
+    //        }
+    //    }
 
-        private static readonly uint _ResultTag;
-        private static MapField<TKey, TValue>.Codec _ResultCodec;
-        static DomainGrpcDictionaryResponse()
-        {
-            _ResultTag = WireFormat.MakeTag(4, WireFormat.WireType.LengthDelimited);
-            FieldCodec<TKey> keyCodec;
-            FieldCodec<TValue> valueCodec;
-            {
-                var codeGenerator = MessageBuilder.GetCodeGenerator<TKey>();
-                if (codeGenerator == null)
-                    codeGenerator = new ObjectCodeGenerator<TKey>();
-                keyCodec = codeGenerator.CreateFieldCodec(1);
-            }
-            {
-                var codeGenerator = MessageBuilder.GetCodeGenerator<TValue>();
-                if (codeGenerator == null)
-                    codeGenerator = new ObjectCodeGenerator<TValue>();
-                valueCodec = codeGenerator.CreateFieldCodec(2);
-            }
-            _ResultCodec = new MapField<TKey, TValue>.Codec(keyCodec, valueCodec, WireFormat.MakeTag(4, WireFormat.WireType.LengthDelimited));
-        }
-    }
+    //    private static readonly uint _ResultTag;
+    //    private static MapField<TKey, TValue>.Codec _ResultCodec;
+    //    static DomainGrpcDictionaryResponse()
+    //    {
+    //        _ResultTag = WireFormat.MakeTag(4, WireFormat.WireType.LengthDelimited);
+    //        FieldCodec<TKey> keyCodec;
+    //        FieldCodec<TValue> valueCodec;
+    //        {
+    //            var codeGenerator = MessageBuilder.GetCodeGenerator<TKey>();
+    //            if (codeGenerator == null)
+    //                codeGenerator = new ObjectCodeGenerator<TKey>();
+    //            keyCodec = codeGenerator.CreateFieldCodec(1);
+    //        }
+    //        {
+    //            var codeGenerator = MessageBuilder.GetCodeGenerator<TValue>();
+    //            if (codeGenerator == null)
+    //                codeGenerator = new ObjectCodeGenerator<TValue>();
+    //            valueCodec = codeGenerator.CreateFieldCodec(2);
+    //        }
+    //        _ResultCodec = new MapField<TKey, TValue>.Codec(keyCodec, valueCodec, WireFormat.MakeTag(4, WireFormat.WireType.LengthDelimited));
+    //    }
+    //}
 }
--- test/Wodsoft.ComBoost.Grpc.Test/SerializationTest.cs
+++ test/Wodsoft.ComBoost.Grpc.Test/SerializationTest.cs
@@ -1,41 +1,93 @@
-﻿//using Google.Protobuf;
-//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Wodsoft.Protobuf;
+using Xunit;
 
-//namespace Wodsoft.ComBoost.Grpc.Test
-//{
-//    public class SerializationTest
-//    {
-//        [Fact]
-//        public void Simply_Class_Test()
-//        {
-//            var type = DomainGrpcTypeBuilder.GetGrpcType<SimplyObject>();
+namespace Wodsoft.ComBoost.Grpc.Test
+{
+    public class SerializationTest
+    {
+        [Fact]
+        public void Simply_Class_Test()
+        {
+            //DomainGrpcArgument<Guid, string> argument = new DomainGrpcArgument<Guid, string>()
+            //{
+            //    Argument1 = new Guid("a089ee87-46b3-424a-8868-8584b9607881"),
+            //    Argument2 = "AQAAAAEAACcQAAAAEKGHgbY43PS0yOoO3gU19ML1z1Mn+MG9pP7ok3WbWtyagqkOuBuVCnEarjuxubK97Q=="
+            //};
+            //DomainGrpcRequest<DomainGrpcArgument<Guid, string>> request = new DomainGrpcRequest<DomainGrpcArgument<Guid, string>>
+            //{
+            //    Argument = argument,
+            //    OS = "Microsoft Windows NT 10.0.19042.0"
+            //};
+            //var data = method.RequestMarshaller.Serializer(request);
+            //var data2 = Convert.FromHexString("0A214D6963726F736F66742057696E646F7773204E542031302E302E31393034322E3022680A5441514141414145414143635141414141454B47486762593433505330794F6F4F33675531394D4C317A314D6E2B4D47397050376F6B3357625774796167716B4F75427556436E4561726A757875624B3937513D3D121087EE89A0B3464A4288688584B9607881");
+            //Assert.Equal(data, data2);
+            //var value = method.RequestMarshaller.Deserializer(data2);
 
-//            //var generator = new Lokad.ILPack.AssemblyGenerator();
-//            //// for ad-hoc serialization
-//            //var bytes = generator.GenerateAssemblyBytes(DomainGrpcTypeBuilder.GetAssembly());
-//            //File.WriteAllBytes("dynamic.dll", bytes);
+            //{
+            //    var method = DomainGrpcMethod<DomainGrpcRequest<DomainGrpcArgument<Guid, string>>, DomainGrpcResponse>.CreateMethod("test", "test");
+            //    DomainGrpcResponse response = new DomainGrpcResponse
+            //    {
+            //        Exception = new DomainGrpcException(new ArgumentException("Test")),
+            //        OS = "UnitTest"
+            //    };
+            //    var data = method.ResponseMarshaller.Serializer(response);
+            //}
+            //{
+            //    var method = DomainGrpcMethod<DomainGrpcRequest, DomainGrpcResponse<string>>.CreateMethod("test", "test");
+            //    DomainGrpcResponse<string> response = new DomainGrpcResponse<string>
+            //    {
+            //        OS = "UnitTest"
+            //    };
+            //    var data = method.ResponseMarshaller.Serializer(response);
+            //}
 
-//            SimplyObject obj = new SimplyObject
-//            {
-//                IntValue = 100,
-//                DoubleValue = 222.222,
-//                StringValue = "This is a simply object"
-//            };
+            {
+                DomainGrpcResponse<string> model = new DomainGrpcResponse<string>
+                {
+                    OS = "Test OS",
+                    Result = "Test"
+                };
+                var data = Message.SerializeToBytes(model);
+                var model2 = Message.DeserializeFromBytes<DomainGrpcResponse<string>>(data);
+                Assert.Equal(model.OS, model.OS);
+                Assert.Equal(model.Result, model.Result);
+            }
 
-//            DomainGrpcType<SimplyObject> grpcObject = (DomainGrpcType<SimplyObject>)Activator.CreateInstance(type);
-//            IMessage message = grpcObject;
-//            var size = message.CalculateSize();
+            {
+                DomainGrpcResponse<List<string>> model = new DomainGrpcResponse<List<string>>
+                {
+                    OS = "Test OS",
+                    Result = new List<string> { "A", "B", "C" }
+                };
+                var data = Message.SerializeToBytes(model);
+                var model2 = Message.DeserializeFromBytes<DomainGrpcResponse<List<string>>>(data);
+                Assert.Equal(model.OS, model.OS);
+                Assert.Equal(model.Result, model.Result);
+            }
 
-//            byte[] data = new byte[message.CalculateSize()];
-//            using (CodedOutputStream cos = new CodedOutputStream(data))
-//            {
-//                message.WriteTo(cos);
-//            }
-//        }
-//    }
-//}
+            {
+                DomainGrpcResponse<string[]> model = new DomainGrpcResponse<string[]>
+                {
+                    OS = "Test OS",
+                    Result = new string[] { "A", "B", "C" }
+                };
+                var data = Message.SerializeToBytes(model);
+                var model2 = Message.DeserializeFromBytes<DomainGrpcResponse<string[]>>(data);
+                Assert.Equal(model.OS, model.OS);
+                Assert.Equal(model.Result, model.Result);
+            }
+
+            //var generator = new Lokad.ILPack.AssemblyGenerator();
+            //// for ad-hoc serialization
+            //var bytes = generator.GenerateAssemblyBytes(DomainGrpcTypeBuilder.GetAssembly());
+            //File.WriteAllBytes("dynamic.dll", bytes);
+
+        }
+    }
+}
