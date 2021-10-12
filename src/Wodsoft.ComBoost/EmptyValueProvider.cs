412840b63cdeecc34d389d1da043f9fbc0495976
Kation <kation@outlook.com>
2021-10-12 18:12:08 +0800

    Convert to string first if not support convert directly

--- src/Wodsoft.ComBoost/EmptyValueProvider.cs
+++ src/Wodsoft.ComBoost/EmptyValueProvider.cs
@@ -75,10 +75,18 @@ namespace Wodsoft.ComBoost
             }
             if (value == null)
                 return null;
-            if (!valueType.IsAssignableFrom(value.GetType()))
+            var currentType = value.GetType();
+            if (!valueType.IsAssignableFrom(currentType))
             {
                 var converter = TypeDescriptor.GetConverter(valueType);
-                value = converter.ConvertFrom(value);
+                if (converter.CanConvertFrom(currentType))
+                    value = converter.ConvertFrom(value);
+                else
+                {
+                    var currentConverter = TypeDescriptor.GetConverter(currentType);
+                    var currentStringValue = currentConverter.ConvertToString(value);
+                    value = converter.ConvertFromString(currentStringValue);
+                }
             }
             return value;
         }
