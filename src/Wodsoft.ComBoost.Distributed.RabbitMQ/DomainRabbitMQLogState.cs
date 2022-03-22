545f359fbe24a683295c28077426dc2d5a5a4fb3
Kation <kation@outlook.com>
2022-03-22 14:54:33 +0800

    Add KeyValuePair enumerable

--- src/Wodsoft.ComBoost.Distributed.RabbitMQ/DomainRabbitMQLogState.cs
+++ src/Wodsoft.ComBoost.Distributed.RabbitMQ/DomainRabbitMQLogState.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Wodsoft.ComBoost.Distributed.RabbitMQ
 {
-    public class DomainRabbitMQLogState
+    public class DomainRabbitMQLogState : IEnumerable<KeyValuePair<string, object>>
     {
         public DomainRabbitMQLogState(string eventType, string exchange, bool redelivered, ulong deliveryTag)
         {
@@ -21,5 +22,18 @@ namespace Wodsoft.ComBoost.Distributed.RabbitMQ
         public ulong DeliveryTag { get; }
 
         public string EventType { get; }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            yield return new KeyValuePair<string, object>("RabbitMQRedelivered", Redelivered);
+            yield return new KeyValuePair<string, object>("RabbitMQExchange", Exchange);
+            yield return new KeyValuePair<string, object>("RabbitMQDeliveryTag", DeliveryTag);
+            yield return new KeyValuePair<string, object>("RabbitMQEventType", EventType);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
--- src/Wodsoft.ComBoost/DomainServiceInvokerLogState.cs
+++ src/Wodsoft.ComBoost/DomainServiceInvokerLogState.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 
 namespace Wodsoft.ComBoost
 {
-    public class DomainServiceInvokerLogState
+    public class DomainServiceInvokerLogState : IEnumerable<KeyValuePair<string, object>>
     {
         public DomainServiceInvokerLogState(Type service, MethodInfo? method)
         {
@@ -16,5 +17,16 @@ namespace Wodsoft.ComBoost
         public Type Service { get; }
 
         public MethodInfo? Method { get; }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            yield return new KeyValuePair<string, object>("DomainService", Service);
+            yield return new KeyValuePair<string, object>("DomainMethod", Method);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
