a7000fb94c5bc4eb64263bac3b4882941f7e5e80
Kation <kation@outlook.com>
2023-05-17 17:00:34 +0800

    Enum

---
+++ test/Wodsoft.ComBoost.Test.Common/InvokeType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wodsoft.ComBoost.Test
+{
+    public enum InvokeType : ulong
+    {
+        None = 0,
+        Test = 5
+    }
+}
