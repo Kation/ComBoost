fbf79b45048ba5bc73bd6191e51e802c5b6e4582
Kation <kation@outlook.com>
2024-08-13 10:59:15 +0800

    Improve grpc invoke exception

--- src/Wodsoft.ComBoost.Grpc.Client/DomainGrpcInvokeException.cs
+++ src/Wodsoft.ComBoost.Grpc.Client/DomainGrpcInvokeException.cs
@@ -4,17 +4,21 @@ using System.Text;
 
 namespace Wodsoft.ComBoost.Grpc.Client
 {
-    public class DomainGrpcInvokeException : Exception
+    public class DomainGrpcInvokeException : Exception, IDomainRpcException
     {
         public DomainGrpcInvokeException(DomainGrpcException exception) : base(exception.Message, exception.InnerException == null ? null : new DomainGrpcInvokeException(exception.InnerException))
         {
             Source = exception.Source;
             Title = exception.Title;
-            StackTrace = exception.StackTrace;
+            RemoteStackTrace = exception.StackTrace;
         }
 
         public string Title { get; }
 
-        public new string StackTrace { get; }
+        public string RemoteStackTrace { get; }
+
+        string IDomainRpcException.StackTrace => RemoteStackTrace;
+
+        IDomainRpcException? IDomainRpcException.InnerException => (DomainGrpcInvokeException?)InnerException;
     }
 }
